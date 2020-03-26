d7da7d3ab17ad11d7475c5350fe890ebe4b1aa50
sanskarkhare <sanskarkhare47@gmail.com>
2020-03-26 22:33:51 +0545

    adding starting project

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = prompt("What is your age.");
+if (Number(age) > 18){
+	alert("powering on, enjoy the ride.");
+}ar age = prompt("What is your age?");
+if (Number(age) < 18){
+	alert("sorry! you are young to drive the car.");
+} 
+if (Number(age) = 18){
+	alert("Enjoy! your first ride.");
+}
